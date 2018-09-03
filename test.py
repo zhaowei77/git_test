6c71b895b28258d0535dbaeb1b3170167a615b74
Haozhaowei <hao_zhaowei@163.com>
2018-09-03 21:18:08 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class A(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def fun(self):
+        pass
